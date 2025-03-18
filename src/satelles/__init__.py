a41ca19bd477d8cef954a5c4c4c173eaa779adef
michealroberts <michael@observerly.com>
2025-03-18 15:31:14 +0000

    chore: amend project satelles configuration across workspace
    chore: amend project satelles configuration across workspace

--- src/satelles/__init__.py
+++ src/satelles/__init__.py
@@ -1,6 +1,6 @@
 # **************************************************************************************
 
-# @package        astral-uv-python-package-template
+# @package        satelles
 # @license        MIT License Copyright (c) 2025 Michael J. Roberts
 
 # **************************************************************************************
