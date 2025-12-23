762288e2992d4b35d03848376f655a6a1c46eeac
michealroberts <michael@observerly.com>
2025-12-23 09:50:07 +0000

    feat: add ECEF reference frame to frames in satelles module
    feat: add ECEF reference frame to frames in satelles module

--- src/satelles/frames.py
+++ src/satelles/frames.py
@@ -8,6 +8,7 @@
 from .body import Body
 from .frame import Frame, Reference
 from .transforms import (
+    ecef_to_eci_transform_provider,
     identity_transform_provider,
 )
 
@@ -20,6 +21,17 @@ ECI = Frame(
     parent=None,
     transform_to_parent=identity_transform_provider,
     name="Earth Centered Inertial",
+)
+
+# **************************************************************************************
+
+ECEF = Frame(
+    reference=Reference.ECEF,
+    origin=Body.EARTH,
+    is_inertial=False,
+    parent=ECI,
+    transform_to_parent=ecef_to_eci_transform_provider,
+    name="Earth Centered Earth Fixed",
 )
 
 # **************************************************************************************
