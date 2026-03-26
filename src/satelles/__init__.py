a922b206ae7cda7bac81c0288a859f53b15fbf56
michealroberts <michael@observerly.com>
2026-03-26 20:25:57 +0000

    feat: add get_rotation_matrix_x to matrix in satelles module
    feat: add get_rotation_matrix_x to matrix in satelles module

--- src/satelles/__init__.py
+++ src/satelles/__init__.py
@@ -68,6 +68,7 @@ from .kepler import (
 )
 from .matrix import (
     Matrix3x3,
+    get_rotation_matrix_x,
 )
 from .mjd import (
     UNIX_EPOCH_AS_MODIFIED_JULIAN_DATE,
@@ -179,6 +180,7 @@ __all__: list[str] = [
     "get_semi_latus_rectum",
     "get_semi_major_axis",
     "get_true_anomaly",
+    "get_rotation_matrix_x",
     "identity_transform_provider",
     "is_visible",
     "normalise",
